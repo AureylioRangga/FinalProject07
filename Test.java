3ecd5b7977fd45fd8566c3bedf69819062927522
Aureylio <aureyliorangga2@gmail.com>
2025-09-25 12:35:26 +0700

    tambah angka 2

--- Test.java
+++ Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Hello, GitHub collaboration!");
+        System.out.println("Hello, GitHub collaboration 2!");
     }
 }   
