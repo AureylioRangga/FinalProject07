c65113196487ece431ef374b543487720dd5a581
Daffaazzi <azzidaffa@gmail.com>
2025-09-25 14:27:31 +0700

    hello

--- Aureylio.java
+++ Aureylio.java
@@ -1,6 +1,6 @@
 public class Aureylio {
     public static void main(String[] args) {
         System.out.println("Hello, Aureylio!");
-        System.out.println("Ini adalah commit saya dari laptop legion");
+        System.out.println("Ini adalah commit saya dari laptop");
     }
 }
